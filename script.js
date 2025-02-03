6d439d446e985ab8c13b4bec07e64c08ea76cc7f
Kenechukwu Akupue <kenechukwuakupueofficial@gmail.com>
2025-02-03 17:24:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showBar(){
+    const sidebar = document.querySelector('.side-menu');
+    sidebar.style.display = 'flex'
+}
+function hideBar(){
+    const sidebar = document.querySelector('.side-menu');
+    sidebar.style.display = 'none'
+}
